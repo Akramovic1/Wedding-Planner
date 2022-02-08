fb441c8b0c1109fdb8bd12fed17b6a696bd43d8a
Amr Momtaz <61145262+AmrMomtaz@users.noreply.github.com>
2022-02-08 15:34:17 +0200

    Small changes

--- src/main/java/com/example/weddingplanner/dao/CartDAO.java
+++ src/main/java/com/example/weddingplanner/dao/CartDAO.java
@@ -7,19 +7,19 @@ import java.util.List;
 
 public interface CartDAO{
 
-    // Adds item to user cart
+    // Adds item to user cart returns insertion state
     int addCartItem(int userID, CartItem cartItem);
 
-    // Remove item from user cart
+    // Remove item from user cart returns deletion state
     int removeCartItem(int userID,CartItem cartItem);
 
-    // Remove all cart items
+    // Remove all cart items returns deletion state
     int clearCart(int userID);
 
     // Returns list of user cart items
     List<CartItem> listCart(int userID);
 
-    // Confirm user purchase
+    // Confirm user purchase Returns Order ID returns order ID
     int confirmPurchase(int userID);
 
     // Get user orders
--- src/main/java/com/example/weddingplanner/model/serviceComponent/Order.java
+++ src/main/java/com/example/weddingplanner/model/serviceComponent/Order.java
@@ -9,4 +9,52 @@ public class Order {
     List<Integer> servicesID;
     Date paymentDate;
     String paymentMethod;
+
+    public Order(int ID, int userID, List<Integer> servicesID, Date paymentDate, String paymentMethod) {
+        this.ID = ID;
+        this.userID = userID;
+        this.servicesID = servicesID;
+        this.paymentDate = paymentDate;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public List<Integer> getServicesID() {
+        return servicesID;
+    }
+
+    public void setServicesID(List<Integer> servicesID) {
+        this.servicesID = servicesID;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
